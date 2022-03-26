c57abe8af99c30befcd3bd36acfccb7d406b08d3
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-03-25 19:49:20 -0500

    Update translate for changed API response
    Also add type annotations and clean up code.

--- translate.py
+++ translate.py
@@ -2,72 +2,66 @@
 # Tiny translator module using sneeky tricks from google's dictionary chrome extention
 # -*- coding: utf-8 -*-
 
+"Translate: Tiny translator module"
+
 # Modified extensively by CoolCat467 from code in https://github.com/ssut/py-googletrans/issues/268
 
 __title__ = 'Tiny tranlator module'
 __author__ = 'CoolCat467'
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 __ver_major__ = 0
-__ver_minor__ = 0
-__ver_patch__ = 2
+__ver_minor__ = 1
+__ver_patch__ = 0
 
 import asyncio
+from urllib.parse import urlencode
+from typing import Final, Any, Callable
 
+import requests
 import aiohttp
 import async_timeout
 
-import requests
 
-from urllib.parse import urlencode
-
-TIMEOUT = 30
-MAGICHEADERS = {
+TIMEOUT: Final[int] = 30
+MAGIC_HEADER: Final[dict] = {
+    # pylint: disable=line-too-long
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
 }
 
-class ResponseError(Exception):
-    pass
-
-def get_translation_url(sentance, tolanguage, fromlanguage='auto'):
-    """Return the url you should visit to get sentance translated to language tolanguage."""
+def get_translation_url(sentance: str, to_language: str, source_language: str='auto') -> str:
+    "Return the url you should visit to get query translated to language to_language."
     query = {'client': 'dict-chrome-ex',
-             'sl'    : fromlanguage,
-             'tl'    : tolanguage,
+             'sl'    : source_language,
+             'tl'    : to_language,
              'q'     : sentance}
-    url = 'https://clients5.google.com/translate_a/t?'+urlencode(query)
-    return url
+    return 'https://clients5.google.com/translate_a/t?'+urlencode(query)
 
-def translate(sentance, tolang, fromlang='auto'):
-    """Use the power of sneeky tricks from chrome browser to do translation."""
+def process_response(result: list) -> str:
+    "Return string after processing response."
+##    return request_result['alternative_translations'][0]['alternative'][0]['word_postproc']
+    return result[0]
+
+def translate_sync(sentance: str, to_lang: str, source_lang: str='auto') -> str:
+    "Use the power of sneeky tricks from chrome browser to do translation."
     # Get url from function, which uses urllib to generate proper query
-    url = get_translation_url(sentance, tolang, fromlang)
-    try:
-        # Make a get request to translate url with magic headers
-        # that make it work right cause google is smart and looks at that.
-        # Also, make request result be json so we can look at it easily
-        request_result = requests.get(url, headers=MAGICHEADERS).json()
-    except:
-        # If it broke somehow, try again
-        # NOTE: could cause indefinite recursion
-        return translate(sentance, tolang, fromlang)
-    # After we get the result, get the right field of the response and return that.
-    # If result field not in request result
-    if not 'alternative_translations' in request_result:
-        # Raise an exception
-        raise ResponseError('Invalid response, likely from an invalid language code')
-    return request_result['alternative_translations'][0]['alternative'][0]['word_postproc']
+    url = get_translation_url(sentance, to_lang, source_lang)
+    # Make a get request to translate url with magic headers
+    # that make it work right because google is smart and looks at that.
+    # Also, make request result be json so we can look at it easily
+    request_result = requests.get(url, headers=MAGIC_HEADER).json()
+    return process_response(request_result)
 
-async def get_translated_coroutine(loop, sentance, tolang, fromlang='auto'):
-    """Return the sentance translated, asyncronously."""
+async def get_translated_coroutine(loop, sentance: str, to_lang: str, source_lang='auto') -> str:
+    "Return the sentance translated, asyncronously."
     # Make a session with our event loop and the magic headers that make it work
     # right because google is smart and looks at that and expects us to be
     # the google translate dictionary extention running in google chrome.
-    async with aiohttp.ClientSession(loop=loop, headers=MAGICHEADERS) as session:
+    async with aiohttp.ClientSession(loop=loop, headers=MAGIC_HEADER) as session:
         # Make sure we have a timeout, so that in the event of network failures
         # or something code doesn't get stuck
         async with async_timeout.timeout(TIMEOUT):
             # Get url from function, which uses urllib to generate proper query
-            url = get_translation_url(sentance, tolang, fromlang)
+            url = get_translation_url(sentance, to_lang, source_lang)
             # Go to that url and get our translated response
             async with session.get(url) as response:
                 # Wait for our response and make it json so we can look at
@@ -77,46 +71,45 @@ async def get_translated_coroutine(loop, sentance, tolang, fromlang='auto'):
                 response.close()
         # Close session
         await session.close()
-    # Get the result from the request result json
-    # If result field not in request result
-    if not 'alternative_translations' in request_result:
-        # Raise an exception
-        raise ResponseError('Invalid response, likely from an invalid language code')
-    translated = request_result['alternative_translations'][0]['alternative'][0]['word_postproc']
-    # Return that
-    return translated
+    return process_response(request_result)
 
-async def translate_async(loop, sentances, tolang, fromlang='auto'):
-    """Translate multiple sentances asyncronously."""
+async def translate_async(loop, sentances: list, to_lang: str, source_lang: str='auto') -> list:
+    "Translate multiple sentances asyncronously."
     # Get a bunch of tasks running at once...
-    coros = [get_translated_coroutine(loop, sentance, tolang, fromlang) for sentance in sentances]
+    coros = [get_translated_coroutine(loop, q, to_lang, source_lang) for q in sentances]
     # then wait for all of them to finish. also asyncio.gather is awesome and
     # puts everything in order correctly somehow whew glad i don't have to do that
     return await asyncio.gather(*coros)
 
-def translate_sentances(sentances, tolang, fromlang='auto'):
-    """Translate many sentances at once using the power of asyncronous code."""
+def translate_sentances(sentances: list, to_lang: str, source_lang: str='auto') -> list:
+    "Translate many sentances at once using the power of asyncronous code."
     # Get asyncronous event loop
-    event_loop = asyncio.get_event_loop()
+    event_loop = asyncio.new_event_loop()
     # Run the main function until it's done and get our translated sentances
     data = event_loop.run_until_complete(
-        translate_async(event_loop, sentances, tolang, fromlang)
+        translate_async(event_loop, sentances, to_lang, source_lang)
     )
+    # Close event loop
+    event_loop.close()
     # Return translated sentances
     return data
 
-def run():
-    """Demonstrate code usage and the power of asynchronous code."""
-    print('\nHello. Welcome to our showing of how asynchronous code is faster than concurrent code.')
+def run() -> None:
+    "Demonstrate code usage and the power of asynchronous code."
+    if input('\nWould you like a demonstration of the translate module? (y/N) : ').lower() != 'y':
+        return
+    print('\nHello. Welcome to the translation demo.')
     
     # Quick make a nice little function stopwatch wrapper
+    # pylint: disable=import-outside-toplevel
     import time
     from functools import wraps
-    def Timed(func):
+    
+    def timed(func) -> Callable[[Callable[[Any], Any]], Any]:
         @wraps(func)
-        def time_func(*args, **kwargs):
+        def time_func(*args, **kwargs) -> Any:
             start = time.perf_counter()
-            result = func()
+            result = func(*args, **kwargs)
             stop = time.perf_counter()
             print(f'\n{func.__name__} took {stop-start:.4f} secconds.\n')
             return result
@@ -132,33 +125,24 @@ def run():
     print('We will be translating the following sentances...\n')
     print('\n'.join(sentances)+'\n')
     # We will be translating from english to french
-    srclang = 'en'
-    dstlang = 'fr'
+    src_lang = 'en'
+    dst_lang = 'fr'
     
-    print(f'... to the language corrosponding with language code "{dstlang}".')
+    print(f'... to the language corrosponding with language code "{dst_lang}".')
     print('\nlettus begin.\n')
     
     # Define two functions, one that translates our sentances one at a time (normaly)
-    @Timed
-    def without_async():
-        print('\n'.join(translate(sentance, dstlang, srclang) for sentance in sentances))
+    @timed
+    def without_async() -> None:
+        print('\n'.join(translate_sync(sentance, dst_lang, src_lang) for sentance in sentances))
     # and another that does it all asyncronously, both timed.
-    @Timed
-    def with_async():
-        print('\n'.join(translate_sentances(sentances, dstlang, srclang)))
+    @timed
+    def with_async() -> None:
+        print('\n'.join(translate_sentances(sentances, dst_lang, src_lang)))
     # Translate the sentances with both to prove asyncronous is better woo
     without_async()
     with_async()
-    
-    # Destroy the evidence! Mostly so nothing silly can happen when used as module.
-    del with_async
-    del without_async
-    del Timed
-    del wraps
-    del time
-    
-    print('Now you know the way. dew it.')
 
 if __name__ == '__main__':
-    print('%s v%s\nProgrammed by %s.' % (__title__, __version__, __author__))
+    print(f'{__title__} v{__version__}\nProgrammed by {__author__}.')
     run()
